8f11c15b476395c2ac7ab9f6ee7942ddd84c9cad
khaled hossain <khaled.hossain@appnap.io>
2023-05-01 12:16:22 +0600

    Add menu component

---
+++ components/ui/menu/PrimaryButton.stories.js
@@ -0,0 +1,14 @@
+import PrimaryBtn from ".";
+
+export default {
+  title: "Buttons/Primary Button",
+  component: PrimaryBtn,
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args: {
+    btnText: "Primary Button",
+    className: "w-full h-[3.5rem] mt-[1.875rem]",
+  },
+};
