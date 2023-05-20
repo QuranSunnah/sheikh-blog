a2310c0e6c17564a5dfb67f9df68d0c8a56bdbd1
Md-Rohed <mdrohed85@gmail.com>
2023-05-21 01:44:28 +0600

    Storybook Added in TopNav Component

---
+++ components/section/TopNav/TopNav.stories.js
@@ -0,0 +1,14 @@
+import TopNav from "./TopNav";
+
+export default {
+  title: "ui/TopNav",
+  component: TopNav,
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args: {
+    btnText: "Top Nav",
+    className: "w-full h-[3.5rem] mt-[1.875rem]",
+  },
+};
